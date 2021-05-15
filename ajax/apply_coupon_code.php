265d2967ff0b032d3c130fa063113e6ecd8f3c64
Koiki Damilare <koikidamilare@gmail.com>
2021-05-15 18:22:39 +0100

    Prevent redirect to Okra if wallet more than total

--- ajax/apply_coupon_code.php
+++ ajax/apply_coupon_code.php
@@ -4,14 +4,16 @@
 	$my_total = $_POST['total'];
 	header('Content-Type: application/json');
 	$get_code = get_one_row_from_one_table('coupon_code', 'coupon_code', $coupon_code);
-	$discount = $get_code['discount'];
-	$total = $my_total - $discount;
 	$response_array = [];
+
 	if($get_code == null){
 		$response_array = [
 			"status" => "Incorrect code, please try again"
 		];
 	}else{
+		$discount = $get_code['discount'];
+		$total = $my_total - $discount;
+
 		$response_array = [
 			"status"=>"success",
 			"discount" => number_format($discount),
