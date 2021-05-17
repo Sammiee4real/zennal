b754d9d6e37805a192dbbd9279fd7c0d096b09d3
Ogunleye Oluwatosin <ogunleyeoluwatosin2014@gmail.com>
2021-05-17 10:54:37 +0100

    made changes to apply coupon

--- ajax/apply_coupon_code.php
+++ ajax/apply_coupon_code.php
@@ -3,15 +3,15 @@
 	$coupon_code = $_POST['coupon_code'];
 	$my_total = $_POST['total'];
 	header('Content-Type: application/json');
-	$get_code = get_one_row_from_one_table('coupon_code', 'coupon_code', $coupon_code);
-	$discount = $get_code['discount'];
-	$total = $my_total - $discount;
 	$response_array = [];
+	$get_code = get_one_row_from_one_table('coupon_code', 'coupon_code', $coupon_code);
 	if($get_code == null){
 		$response_array = [
 			"status" => "Incorrect code, please try again"
 		];
 	}else{
+		$discount = $get_code['discount'];
+		$total = $my_total - $discount;
 		$response_array = [
 			"status"=>"success",
 			"discount" => number_format($discount),
